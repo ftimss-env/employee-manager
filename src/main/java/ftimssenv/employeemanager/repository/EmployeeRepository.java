89413115028d91d101d8f71833e2660ebaaad807
Fatima Mohamedali <fatimamohamedali@fatimas-mbp.myfiosgateway.com>
2023-04-09 13:53:41 -0400

    created spring data repository

---
+++ src/main/java/ftimssenv/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package ftimssenv.employeemanager.repository;
+
+import ftimssenv.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
